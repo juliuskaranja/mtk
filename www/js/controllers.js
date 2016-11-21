ec7421b5859931bd8e0a4cbe86cf02d028f4ad51
Julius Karanjah <jkaranjah@clemcreativity.com>
2016-11-21 23:11:53 +0300

    correct the plugin url

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -237,6 +237,20 @@ angular.module('starter.controllers', [])
 
         //sell products with barcode..
         $scope.sellWithBarcode = function () {
+            
+            window.plugins.barcodeScanner.scan(
+                function (result) {
+                    alert("We got a barcode\n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+            );
+
+
             cordova.plugins.barcodeScanner.scan(
                 function (result) {
                     alert("We got a barcode\n" +
